5a240f07cef54a17f01187dc161d5bd4bba49ba7
TEN TEERAPAKORN <teerapakorn.meesri@gmail.com>
2024-07-14 14:17:48 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
@@ -6,21 +5,10 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todo-list';
-
-  service_url = 'https://669338d0c6be000fa07a1a18.mockapi.io/todo/v1/topic'
-  topics:any = []
-
-  constructor(private http: HttpClient){
-    http.get(this.service_url).subscribe({
-      next: (result) => {
-        this.topics = result
-      }
-    })
-  }
 }
--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -1,7 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-topic',
+  selector: 'app-root',
   standalone: true,
   imports: [],
   templateUrl: './topic.component.html',
@@ -9,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class TopicComponent {
 
+  service_url = 'https://669338d0c6be000fa07a1a18.mockapi.io/todo/v1/topic'
+  topics:any = []
+
+  constructor(private http: HttpClient) {
+    http.get(this.service_url).subscribe({
+      next: (result) => {
+        this.topics = result
+      }
+    })
+  }
 }
