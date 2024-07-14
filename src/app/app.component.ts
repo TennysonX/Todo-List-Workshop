f9319749d3d2633937db9aa668dd5c31504d2879
TEN TEERAPAKORN <teerapakorn.meesri@gmail.com>
2024-07-14 13:52:55 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todo-list';
+
+  service_url = 'https://669338d0c6be000fa07a1a18.mockapi.io/todo/v1/topic'
+  topics:any = []
+
+  constructor(private http: HttpClient){
+    http.get(this.service_url).subscribe({
+      next: (result) => {
+        this.topics = result
+      }
+    })
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TopicComponent } from './topic/topic.component';
+import { ListComponent } from './list/list.component';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: TopicComponent },
+    { path: 'list', component: ListComponent}
+];
